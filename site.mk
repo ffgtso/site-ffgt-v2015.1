3ed31125a54ad72b6edb50e16fa64bd92231b7c2
wusel42 <wusel+src@uu.org>
2016-05-28 00:55:21 +0200

    Add airtime stats

--- site.mk
+++ site.mk
@@ -34,7 +34,8 @@ GLUON_SITE_PACKAGES := \
 	ffgt-banner \
 	gluon-wificheck \
 	mtr \
-	ffgt-geolocate
+	ffgt-geolocate \
+	gluon-airtime
 
 #	gluon-migrate-vpn \
 #	ffho-autoupdater-wifi-fallback \
@@ -54,7 +55,7 @@ GLUON_SITE_PACKAGES := \
 #         comgt kmod-usb-serial kmod-usb2 kmod-usb-uhci \
 #         kmod-usb-ohci usb-modeswitch \
 
-DEFAULT_GLUON_RELEASE := 0.7.4~0
+DEFAULT_GLUON_RELEASE := 0.7.5~0
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
